2d27a1e205a74fcc73d5d82a1270db8708a75092
Damien Frikha <damien.frikha@outlook.com>
2024-08-14 11:08:53 +0200

    Filtre par nom et nombre de couronnes (fix #2)

---
+++ Debouncer.cs
@@ -0,0 +1,58 @@
+﻿namespace CestlheureduBK;
+
+/// <summary>
+/// Debouncer helps you to debounce asynchronous actions.
+/// You can use it in your callbacks to prevent multiple calls of the same action in a short period of time.
+/// </summary>
+public class Debouncer : IDisposable
+{
+    private CancellationTokenSource? _debounceCancellationTokenSource = null;
+    private bool _disposed;
+
+    /// <summary>
+    /// Starts the debouncing.
+    /// </summary>
+    /// <param name="millisecondsDelay">The delay in milliseconds for debouncing.</param>
+    /// <param name="actionAsync">The asynchronous action to be executed. The <see cref="CancellationToken"/> gets canceled if the method is called again.</param>
+    public async Task DebounceAsync(int millisecondsDelay, Func<CancellationToken, Task> actionAsync)
+    {
+        try
+        {
+            if (_debounceCancellationTokenSource != null)
+            {
+                await _debounceCancellationTokenSource.CancelAsync();
+                _debounceCancellationTokenSource.Dispose();
+            }
+
+            _debounceCancellationTokenSource = new CancellationTokenSource();
+            await Task.Delay(millisecondsDelay, _debounceCancellationTokenSource.Token);
+            await actionAsync(_debounceCancellationTokenSource.Token);
+        }
+        catch (TaskCanceledException)
+        {
+            // NOOP
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposed)
+        {
+            if (disposing)
+            {
+                if (_debounceCancellationTokenSource != null)
+                {
+                    _debounceCancellationTokenSource?.Cancel();
+                    _debounceCancellationTokenSource?.Dispose();
+                }
+            }
+
+            _disposed = true;
+        }
+    }
+}
